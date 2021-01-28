e4c261f37b089bfdfefe528ba40aaea1e49ce0f2
JoseHuertasDev <70613520+JoseHuertasDev@users.noreply.github.com>
2021-01-28 13:33:45 -0300

    Folder refactoring: moved Migrations outside of Data and changed ApplicationDbContext's directory

--- MonkeyRadioStreaming/Models/ApplicationDbContext.cs
+++ MonkeyRadioStreaming/Models/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace MonkeyRadioStreaming.Data
+namespace MonkeyRadioStreaming.Models
 {
     public class ApplicationDbContext : IdentityDbContext
     {
--- MonkeyRadioStreaming/Startup.cs
+++ MonkeyRadioStreaming/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MonkeyRadioStreaming.Areas.Identity;
-using MonkeyRadioStreaming.Data;
+using MonkeyRadioStreaming.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
