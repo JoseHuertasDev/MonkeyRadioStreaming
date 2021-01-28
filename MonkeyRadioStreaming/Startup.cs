83abde2ee6b7b30a053b18b215dfefa00937d2dd
JoseHuertasDev <70613520+JoseHuertasDev@users.noreply.github.com>
2021-01-28 13:23:19 -0300

    Changed SQL Server to SQLite

--- MonkeyRadioStreaming/Startup.cs
+++ MonkeyRadioStreaming/Startup.cs
@@ -32,7 +32,7 @@ namespace MonkeyRadioStreaming
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+                options.UseSqlite(
                     Configuration.GetConnectionString("DefaultConnection")));
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
